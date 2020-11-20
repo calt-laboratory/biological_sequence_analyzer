dc99adf0b63cba1815e71472dc56adf765257592
calt-laboratory <christophalt@web.de>
2020-11-20 07:13:31 +0100

    shortened the docstrings

--- biological_seq_analyzer.py
+++ biological_seq_analyzer.py
@@ -12,8 +12,6 @@ class BioSeqAnalyzer:
     def __init__(self, sequence = 'ACGT', sequence_type = "DNA"):
         """
         Constructor for initialization of a biological sequence and the type of the sequence.
-        :param sequence (str): biological sequence
-        :param sequence_type (str): type of the biological sequence
         """
         self.sequence = sequence.upper()
         self.sequence_type = sequence_type
@@ -21,9 +19,6 @@ class BioSeqAnalyzer:
     def generate_sequence(self, nucleotides = "ACGT", sequence_type = "DNA", sequence_length = 100):
         """
         Returns a string representing the randomly generated biological sequence.
-        :param nucleotides (str):
-        :param sequence_length:
-        :return: str
         """
         random_sequence = "".join([random.choice(nucleotides) for i in range(sequence_length)])
         self.__init__(random_sequence, sequence_type)
@@ -31,21 +26,18 @@ class BioSeqAnalyzer:
     def sequence_length(self):
         """
         Returns an integer representing the length of a biological sequence.
-        :return: int
         """
         return len(self.sequence)
 
     def get_seq_info(self):
         """
         Returns 2 strings representing the biological sequence and the type of the sequence.
-        :return: string
         """
         return 'Sequence: {0}\nSequence type: {1}\n'.format(self.sequence, self.sequence_type)
 
     def nucleotide_freq_counter(self):
         """
         Returns a dictionary comprising the frequency of each nucleotide in a given sequence.
-        :return: dict
         """
         if self.sequence_type == "DNA":
             for i in self.sequence:
@@ -59,21 +51,18 @@ class BioSeqAnalyzer:
     def gc_content(self):
         """
         Returns a float representing the GC content of a biological sequence.
-        :return: float
         """
         return round(((self.sequence.count('C') + self.sequence.count('G')) * 100) / len(self.sequence), 2)
 
     def reverse_sequence(self):
         """
         Returns a string representing the reverse version of the input biological sequence.
-        :return: str
         """
         return self.sequence[::-1]
 
     def reverse_complement_sequence(self):
         """
         Returns a string representing the reverse complement of a biological sequence.
-        :return: str
         """
         if self.sequence_type == "DNA":
             return ''.join([dna_complement_dict[nuc] for nuc in self.sequence])[::-1]
@@ -83,7 +72,6 @@ class BioSeqAnalyzer:
     def dna_2_rna(self):
         """
         Returns a string representing the RNA sequence translated from a DNA sequence.
-        :return: str
         """
         if self.sequence_type == "DNA":
             return self.sequence.replace('T', 'U')
@@ -93,9 +81,6 @@ class BioSeqAnalyzer:
     def codon_bias(self, amino_acid):
         """
         Returns a dictionary representing the frequency of codons of a given amino acid.
-        :param dna_string (str): DNA string
-        :param amino_acid:
-        :return: dict
         """
         codon_list = []
         if self.sequence_type == "DNA":
