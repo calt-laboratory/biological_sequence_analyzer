323f1eacc0971260b52bc8c7b9d07a808849b0f0
calt-laboratory <christophalt@web.de>
2023-03-01 02:23:49 +0100

    added type hints

--- biological_seq_analyzer.py
+++ biological_seq_analyzer.py
@@ -1,58 +1,60 @@
 ##### Class for basic DNA sequence analysis
 ##### Author: Christoph Alt
 
-# Import files
-from biological_seq_dictionaries import *
-
 # Import modules
 import random
 from collections import Counter
+from typing import Dict, Union
+
 import matplotlib.pyplot as plt
+
+# Import files
+from biological_seq_dictionaries import *
 
 class BioSeqAnalyzer:
     """
     Class for analyzing biological sequences (RNA, DNA).
     """
 
-    def __init__(self, sequence = 'ACGT', sequence_type = "DNA"):
+    def __init__(self, sequence: str = "ACGT", sequence_type: str = "DNA") -> None:
         """
         Constructor for initialization of a biological sequence and the type of the sequence.
         """
         self.sequence = sequence.upper()
         self.sequence_type = sequence_type
 
-    def __repr__(self):
+    def __repr__(self) -> None:
         """
         Returns a formal representation of an instanced object as string.
         """
         print(f'BiolgicalSeqAnalyzer(sequence={self.sequence}, sequence_type={self.sequence_type}')
 
-    def __str__(self):
+    def __str__(self) -> None:
         """
         Returns an informal representation of an instanced as string.
         """
         print(f'Biolgical Sequence Analyzer: Seq = {self.sequence}, Seq_Type = {self.sequence_type}')
 
-    def generate_sequence(self, nucleotides = "ACGT", sequence_type = "DNA", sequence_length = 100):
+    def generate_sequence(self, nucleotides: str = "ACGT", sequence_type: str = "DNA", sequence_length: str = 100):
         """
         Returns a string representing the randomly generated biological sequence.
         """
         random_sequence = "".join([random.choice(nucleotides) for i in range(sequence_length)])
         self.__init__(random_sequence, sequence_type)
 
-    def sequence_length(self):
+    def sequence_length(self) -> int:
         """
         Returns an integer representing the length of a biological sequence.
         """
         return len(self.sequence)
 
-    def get_seq_info(self):
+    def get_seq_info(self) -> str:
         """
         Returns 2 strings representing the biological sequence and the type of the sequence.
         """
         return 'Sequence: {0}\nSequence type: {1}\n'.format(self.sequence, self.sequence_type)
 
-    def nucleotide_freq_counter(self):
+    def nucleotide_freq_counter(self) -> Dict[str, int]:
         """
         Returns a dictionary comprising the frequency of each nucleotide in a given sequence.
         """
@@ -65,19 +67,19 @@ class BioSeqAnalyzer:
                 rna_nucleotide_dict[i] += 1
             return rna_nucleotide_dict
 
-    def gc_content(self):
+    def gc_content(self) -> float:
         """
         Returns a float representing the GC content of a biological sequence.
         """
         return round(((self.sequence.count('C') + self.sequence.count('G')) * 100) / len(self.sequence), 2)
 
-    def reverse_sequence(self):
+    def reverse_sequence(self) -> str:
         """
         Returns a string representing the reverse version of the input biological sequence.
         """
         return self.sequence[::-1]
 
-    def reverse_complement_sequence(self):
+    def reverse_complement_sequence(self) -> str:
         """
         Returns a string representing the reverse complement of a biological sequence.
         """
@@ -86,7 +88,7 @@ class BioSeqAnalyzer:
         elif self.sequence_type == "RNA":
             return ''.join([rna_complement_dict[nuc] for nuc in self.sequence])[::-1]
 
-    def dna_2_rna(self):
+    def dna_2_rna(self) -> str:
         """
         Returns a string representing the RNA sequence translated from a DNA sequence.
         """
@@ -95,7 +97,7 @@ class BioSeqAnalyzer:
         else:
             return "Given sequence is not a DNA sequence!"
 
-    def codon_bias(self, amino_acid, viz = True):
+    def codon_bias(self, amino_acid: str = "A", viz: bool = True) -> Dict:
         """
         Returns a dictionary representing the frequency of codons of a given amino acid.
         """
@@ -117,17 +119,26 @@ class BioSeqAnalyzer:
         if viz == True:
             labels = list(percentage_codon_dict.keys())
             vals = list(percentage_codon_dict.values())
-            plt.pie(vals, labels = vals , normalize = False)
+            plt.pie(vals, labels=vals , normalize=False)
             plt.legend(labels)
             plt.title(f'Frequency of codons for the amino acid: {amino_acid}')
             plt.show()
         return percentage_codon_dict
 
-    def rna_2_protein(self):
+    def rna_2_protein(self) -> Union[str, None]:
         """
         Returns a string representing a amino acid sequence translated from a RNA sequence.
         """
         if self.sequence_type == "RNA":
+            print(type(''.join([rna_codon_dict[self.sequence[i:i + 3]] for i in range(0, len(self.sequence) - 2, 3)])))
             return ''.join([rna_codon_dict[self.sequence[i:i + 3]] for i in range(0, len(self.sequence) - 2, 3)])
         else:
             print('Given sequence is not a RNA!')
+
+def main():
+    seq_analyzer = BioSeqAnalyzer(sequence="ACUGAUUUUUACCCAAAA", sequence_type="RNA")
+    seq_analyzer.rna_2_protein()
+
+
+if __name__ == "__main__":
+    main()
